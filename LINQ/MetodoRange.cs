588d81424fd5deac50b7d7c0fb134809dbb9276b
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-08-09 09:41:29 -0500

    Add files via upload

---
+++ LINQ/MetodoRange.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class MetodoRange
+    {
+        public void segundometodo ()
+        {
+            Console.WriteLine("---------------------------------------------------");
+
+            IEnumerable<int> squares = Enumerable.Range(1, 10).Select(x => x * x);
+            foreach (int x in squares)
+            Console.Write("[{0}]", x);
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey(true);
+        }
+
+
+
+    }
+}
---
+++ LINQ/PrincipalMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class PrincipalMain
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> pila = new Stack<string>();
+
+            pila.Push("hola");
+            pila.Push("buenas");
+            pila.Push("tardes,");
+            pila.Push("adios");
+            pila.Push("a todos.");
+
+            pila.Pop(); // pop quitar un elemento
+
+
+            Console.WriteLine("Primer elemento: " + pila.Peek()); // peek = saber cual es el primer elemento de la pila
+            
+            pila.Clear();
+
+            pila.
+
+            Console.WriteLine("Numero elementos: " + pila.Count); // count = saber el numero de elementos de la pila
+            
+            //Console.WriteLine("mmmdmdmd" + pila.ToArray());
+
+            foreach (string p in pila)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.WriteLine("\npresione una tecla para seguir");
+            Console.ReadLine();
+        }
+    }
+}
