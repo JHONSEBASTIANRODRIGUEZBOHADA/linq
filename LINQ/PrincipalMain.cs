9a5514153c4d37bc2536f6673d6b8c747fe3f6d7
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-08-09 11:33:14 -0500

    Add files via upload

---
+++ LINQ/PrincipalMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class PrincipalMain
+    {
+        static void Main(string[] args)
+        {
+            //coleccion que almacena elementos de forma LIFO
+            Stack<int> num = new Stack<int>();
+            foreach (int numero in new int[] { 2, 4, 6, 8, 10 })
+            {
+                num.Push(numero);
+
+            }
+
+            Console.WriteLine("\nRecorriendo la pila");
+
+            foreach(int numero in num)
+            {
+                Console.WriteLine("\n" + numero);
+            }
+
+            Console.WriteLine("\nEliminar un elemento");
+
+            num.Pop();
+
+            foreach(int numero in num)
+            {
+                Console.WriteLine("\n" + numero);
+            }
+
+            Console.WriteLine("\nPrecione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
